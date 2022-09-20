c025f624a5852cd4d337124cf440049bea690d05
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-20 21:57:22 +0100

    feat: Passed test

--- phase-2.js
+++ phase-2.js
@@ -1,14 +1,41 @@
-const [addNums, addManyNums] = require("./phase-1");
+const [addNums, addManyNums] = require("./phase-1.js");
 
 // Runs `addNums` in 10 increasing increments
-function addNums10(increment) {
+function addNums10(inc) {
   // Fill this in
+  let answer=[];
+  
+    for(let i=inc; i<=10*inc; i+=inc){
+     answer.push(addNums(i));
 
+    }
+
+
+
+            
+
+
+	return answer;
 }
 
+
+
 // Runs `addManyNums` in 10 increasing increments
-function addManyNums10(increment) {
+function addManyNums10(inc) {
   // Fill this in
+  let answer=[];
+  
+    for(let i=inc; i<=10*inc; i+=inc){
+     answer.push(addManyNums(i));
+
+    }
+
+
+
+            
+
+
+	return answer;
 
 }
 
