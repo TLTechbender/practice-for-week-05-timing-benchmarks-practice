05d8ba91906ae0a6b2ca178ccd77a1d665bc0c7b
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-20 21:56:45 +0100

    feat: Passed test

--- phase-1.js
+++ phase-1.js
@@ -1,16 +1,43 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
   // Fill this in
+   let answer=0
+	for(let i=1; i<=n ; i++){
+    answer+=i;
 
+}
+return answer;
 }
 
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
-  // Fill this in
+ let answer=0;
 
+	for(let i=0; i<=n; i++){
+         answer+= addNums(i);
+
+	}
+return answer;
 }
+
+const startTime = Date.now();
+    addNums(10000000);
+    const endTime = Date.now();
+ console.log(`${endTime} - ${startTime}`);
+
+
+
+
+const StartTime = Date.now();
+addManyNums(100);
+const EndTime = Date.now();
+console.log(`${EndTime} - ${StartTime}`);
 
 
 
 module.exports = [addNums, addManyNums];
+
+
+
+
