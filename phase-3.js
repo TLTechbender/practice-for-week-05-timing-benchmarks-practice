e795abfee037ec4c2368195c65f4b092404a6fda
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-20 21:57:48 +0100

    feat: Passed test

--- phase-3.js
+++ phase-3.js
@@ -1,20 +1,61 @@
 const [addNums, addManyNums] = require("./phase-1");
 
-function addNums10Timing(increment) {
+function addNums10Timing(inc) {
   // Copy your `addNums10` code here
   // Then, add timing code
 
   // Your code here
+	//
+	//Because my functions were calling so many things at the same time, I needed help to use console.time for this.
+let answer=[];
+  
+    for(let i=inc; i<=10*inc; i+=inc){
+        console.time(`Timer for increment ${i}`);
+	    let startTime=Date.now();
+	    let result = addNums(i);
+               let endTime= Date.now();
+
+	    console.timeEnd(`Timer for increment ${i}`);
+	    console.log(`Runtime; ${endTime - startTime}ms`);
+	    answer.push(result);
+    }
+
+
+
+            
+
+
+	return answer;
 
 }
 
 
-function addManyNums10Timing(increment) {
+function addManyNums10Timing(inc) {
 // Copy your `addManyNums10` code here
 // Then, add timing code
 
   // Your code here
+   let answer=[];
+  
+    for(let i=inc; i<=10*inc; i+=inc){
+   	
+     console.time(`Timer for increment ${i}`)
 
+
+	    let startTime= Date.now();
+	    let result = addManyNums(i);
+          let endTime= Date.now();
+          console.timeEnd(`Timer for increment ${i}`);
+	    console.log(`Runtime; ${endTime - startTime}ms`)
+           answer.push(result);
+    }
+
+
+
+            
+
+
+	return answer;
 }
 
 
